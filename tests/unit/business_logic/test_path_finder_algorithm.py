50b683540a28865e12c9c637657469acc3b5d420
David Lesner <lesnerd@gmail.com>
2020-01-14 00:10:23 +0200

    split tests

--- tests/unit/business_logic/test_path_finder_algorithm.py
+++ tests/unit/business_logic/test_path_finder_algorithm.py
@@ -1,25 +1,45 @@
 from services.business_logic.path_finder_algorithm import PathFinderAlgorithm
 
-true_array = [[4, 4, 1, 1, 2, 2, 1000, 1],
-                   [1, 2, 4, 1, 5, 100, 1, 100, 1, 1, 1, 1],
-                   [3, 2, 0, 1],
-                   [2, 0, 1, 1, 0],
-                   [0]]
-
-false_array = [[4, 2, 1, 3, 2, 2, 1000, 1],
-                    [1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9],
-                    [2, 2, 0, 1],
-                    [2, 0, 0, 1, 0]]
 
 class TestPathFinderAlgorithm:
-    def test_find_with_result_true(self):
-        for arr in true_array:
-            if not PathFinderAlgorithm.find(arr):
-                assert False
+    def test_find_given_input_return_true(self):
+        arr = [4, 4, 1, 1, 2, 2, 1000, 1]
+        if not PathFinderAlgorithm.find(arr):
+            assert False
         assert True
 
-    def test_find_with_result_false(self):
-        for arr in false_array:
-            if PathFinderAlgorithm.find(arr):
-                assert False
+    def test_find_one_item_return_true(self):
+        arr = [0]
+        if not PathFinderAlgorithm.find(arr):
+            assert False
+        assert True
+
+    def test_find_straight_to_end_return_true(self):
+        arr = [3, 2, 0, 1]
+        if not PathFinderAlgorithm.find(arr):
+            assert False
+        assert True
+
+    def test_find_can_stuck_if_go_in_wrong_direction_return_true(self):
+        arr = [1, 2, 4, 1, 5, 100, 1, 100, 1, 1, 1, 1]
+        if not PathFinderAlgorithm.find(arr):
+            assert False
+        assert True
+
+    def test_find_given_input_return_false(self):
+        arr = [4, 2, 1, 3, 2, 2, 1000, 1]
+        if PathFinderAlgorithm.find(arr):
+            assert False
+        assert True
+
+    def test_find_with_zeros_return_false(self):
+        arr = [2, 0, 0, 1, 0]
+        if PathFinderAlgorithm.find(arr):
+            assert False
+        assert True
+
+    def test_find_long_input_return_false(self):
+        arr = [1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9]
+        if PathFinderAlgorithm.find(arr):
+            assert False
         assert True
